a771c527d42774cc6f86db76e5b029356986998e
Alex <connectionwithme1@gmail.com>
2026-03-15 10:36:09 +0200

    Completed: 	8.5 Error Handling Part 1 	8.6 Error Handling Part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,59 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import "./index.css";
 
 const KEY = "6683bf1f12284443afa113106260903";
 
 function App() {
-  const [city, setCity] = useState("London");
-  const [weatherData, setWeatherData] = useState(null);
+    const [city, setCity] = useState("London");
+    const [weatherData, setWeatherData] = useState(null);
+    const [error, setError] = useState(null)
 
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`)
-      const data = await res.json();
-      setWeatherData(data);
-    }
+    useEffect(() => {
+        async function getData() {
 
-    getData();
-  }, []);
+            try {
+                const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`);
+                const data = await res.json();
+                if (data.error) {
+                    setError(data.error.message)
+                }
 
-  console.log(weatherData);
+                setWeatherData(data);
+                setWeatherData(null)
+
+            } catch (err) {
+                setError(err.message);
+                setWeatherData(null);
+            }
+        }
 
 
+        getData();
+    }, []);
 
-  return (
-    <div className="app">
-      <div className="widget-container">
-        <div className="weather-card-container">
-          <h1 className="app-title">Weather Widget</h1>
-          <div className="search-container">
-            <input type="text" placeholder="Enter city name" className="search-input" />
-          </div>
+
+    return (
+        <div className="app">
+            <div className="widget-container">
+                <div className="weather-card-container">
+                    <h1 className="app-title">Weather Widget</h1>
+                    <div className="search-container">
+                        <input type="text" placeholder="Enter city name" className="search-input"/>
+                    </div>
+                </div>
+                <div className="weather-card">
+                    <h2>{`${weatherData?.location?.name}`}, {`${weatherData?.location?.country}`}</h2>
+                    <img src="" alt="icon" className="weather-icon"/>
+                    <p className="temperature">11°C</p>
+                    <p className="condition">rainy</p>
+                    <div className="weather-details">
+                        <p>Humidity: 20%</p>
+                        <p>Wind: 22 km/h</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="weather-card">
-          <h2>{`${weatherData?.location.name}`}, {`${weatherData?.location.country}`}</h2>
-          <img src="" alt="icon" className="weather-icon" />
-          <p className="temperature">11°C</p>
-          <p className="condition">rainy</p>
-          <div className="weather-details">
-            <p>Humidity: 20%</p>
-            <p>Wind: 22 km/h</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
