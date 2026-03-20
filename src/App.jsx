6a34131c991df646575f3136a6490f02c3b4c88f
Alex <connectionwithme1@gmail.com>
2026-03-20 07:18:34 +0200

    Completed: 	8.7 Using data received from the API         8.8 How the dependency array works in useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,30 @@ import "./index.css";
 const KEY = "52c4d4f68f1447cdbe0160538261503";
 
 function App() {
-    const [city, setCity] = useState("Kyiv");
-    const [weatherData, setWeatherData] = useState(null);
+    const [city, setCity] = useState('Berlin')
+    const [weatherData, setWeatherData] = useState(null)
     const [error, setError] = useState(null)
 
     useEffect(() => {
         async function getData() {
-
             try {
                 const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`);
                 const data = await res.json();
 
-                if (data.error) {
+                if(data.error) {
                     setError(data.error.message)
-                    setWeatherData(null)
+                    setWeatherData(null);
                     return;
                 }
-
                 setWeatherData(data);
-                setError(null)
-
+                setError(null);
             } catch (err) {
-                setError(err.message);
+                setError(err.message)
                 setWeatherData(null);
             }
         }
-
         getData();
-    }, []);
+    }, [city]);
 
     console.log(weatherData);
 
@@ -41,18 +37,19 @@ function App() {
             <div className="widget-container">
                 <div className="weather-card-container">
                     <h1 className="app-title">Weather Widget</h1>
+                    <p className="app-text">{error}</p>
                     <div className="search-container">
-                        <input type="text" placeholder="Enter city name" className="search-input"/>
+                        <input onChange={(e) => setCity(e.target.value)} type="text" placeholder="Enter city name" className="search-input"/>
                     </div>
                 </div>
                 <div className="weather-card">
-                    <h2>{`${weatherData?.location?.name}`}, {`${weatherData?.location?.country}`}</h2>
-                    <img src="" alt="icon" className="weather-icon"/>
-                    <p className="temperature">11°C</p>
-                    <p className="condition">rainy</p>
+                    <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
+                    <img src={`${weatherData?.current?.condition?.icon}`} alt="icon" className="weather-icon"/>
+                    <p className="temperature">{`${Math.round(weatherData?.current?.temp_c)}`}°C</p>
+                    <p className="condition">{`${weatherData?.current?.condition?.text}`}</p>
                     <div className="weather-details">
-                        <p>Humidity: 20%</p>
-                        <p>Wind: 22 km/h</p>
+                        <p>Humidity: {`${weatherData?.current?.humidity}`}%</p>
+                        <p>Wind: {`${weatherData?.current?.wind_kph}`} km/h</p>
                     </div>
                 </div>
             </div>
